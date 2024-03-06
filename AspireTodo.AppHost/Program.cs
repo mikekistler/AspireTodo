f8950ec761ecf0cc0f7f48839731b0935149b712
Mike Kistler (from Dev Box) <mikekistler@microsoft.com>
2024-03-06 10:36:08 -0800

    Add queue infrastructure

--- AspireTodo.AppHost/Program.cs
+++ AspireTodo.AppHost/Program.cs
@@ -2,10 +2,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.AspireTodo_ApiService>("apiservice");
+var storage = builder.AddAzureStorage("storage").UseEmulator();
+
+var queues = storage.AddQueues("queues");
+
+var apiService = builder.AddProject<Projects.AspireTodo_ApiService>("apiservice")
+    .WithReference(queues);
 
 builder.AddProject<Projects.AspireTodo_Web>("webfrontend")
     .WithReference(cache)
+    .WithReference(queues)
     .WithReference(apiService);
 
 builder.Build().Run();
